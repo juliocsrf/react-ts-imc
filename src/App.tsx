cbdbc0b51456b84c08ef5c6f8217a1fc18f7a6b9
Julio Fonseca <juliocsrmf@gmail.com>
2022-08-20 13:11:31 -0300

    System calc

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,27 @@ import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
 import GridItem from './components/GridItem';
+import leftArrowImage from './assets/leftarrow.png';
 
-import { levels, calculateImc } from './helpers/imc';
+import { levels, calculateImc, Level } from './helpers/imc';
 
 const App = () => {
     const [heightField, setHeightField] = useState<number>(0);
     const [weightField, setWeightField] = useState<number>(0);
+    const [toShow, setToShow] = useState<Level | null>(null);
 
     const handleCalculateButton = () => {
         if (heightField && weightField) {
-            console.log(heightField);
+            setToShow(calculateImc(heightField, weightField));
         } else {
             alert('Digite todos os campos');
         }
+    };
+
+    const handleBackButton = () => {
+        setToShow(null);
+        setHeightField(0);
+        setWeightField(0);
     };
 
     return (
@@ -51,11 +59,22 @@ const App = () => {
                 </div>
 
                 <div className={styles.rightSide}>
-                    <div className={styles.grid}>
-                        {levels.map((item, key) => (
-                            <GridItem key={key} item={item} />
-                        ))}
-                    </div>
+                    {!toShow && (
+                        <div className={styles.grid}>
+                            {levels.map((item, key) => (
+                                <GridItem key={key} item={item} />
+                            ))}
+                        </div>
+                    )}
+
+                    {toShow && (
+                        <div className={styles.rightBig}>
+                            <div className={styles.rightArrow} onClick={handleBackButton}>
+                                <img src={leftArrowImage} alt='' width={25} />
+                            </div>
+                            <GridItem item={toShow} />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
